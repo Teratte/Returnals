6031a7c7faefe5a4552cfed4813f714fe9b574fa
yuno0271 <yuno0271@naver.com>
2025-02-11 13:34:13 +0900

    애니메이션 함수 제거

--- Returnals/Assets/Scripts/Status.cs
+++ Returnals/Assets/Scripts/Status.cs
@@ -7,19 +7,19 @@ public class Status : MonoBehaviour
     [SerializeField]
     private float recoverRate = 5.0f;    // 플레이어 HP 회복 비율
     [SerializeField]
-    private float walkSpeed = 2.0f;
+    private float walkSpeed = 2.0f;      // 걷는 속도
     [SerializeField]
-    private float runSpeed = 5.0f;
+    private float runSpeed = 5.0f;       // 뛰는 속도
     [SerializeField]
-    private float moveSpeed = 4.0f;
+    private float moveSpeed = 4.0f;      // 움직이는 속도
 
-    public float PlayerHP => PlayerHP;
-    public float RecoverRate => RecoverRate;
+    public float PlayerHP => playerHP;
+    public float RecoverRate => recoverRate;
     public float WalkSpeed => walkSpeed;
     public float RunSpeed => runSpeed;
     public float MoveSpeed
     {
-        set => moveSpeed = value;
+        set => moveSpeed = Mathf.Max(0,value);
         get => moveSpeed;
     }
 }
