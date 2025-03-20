ea858a03ea928ebb583e05fa578a501f4ede9446
Jae hun Jin <ospf0306@gmail.com>
2025-03-21 05:41:26 +0900

    맵 복사 및 씬 이동 구현

---
+++ Returnals/Assets/Scripts/Handler/KeyPressHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class KeyPressHandler : MonoBehaviour
+{
+    public string playerTag = "Player"; // 플레이어 태그 설정
+    private bool isPlayerNearby = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            isPlayerNearby = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            isPlayerNearby = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManagerScript.Instance.LoadRandomScene(); // 씬 이동 실행
+        }
+    }
+}
