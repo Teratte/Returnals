5ecb66d7e137e5f2d75d92260537f6764e25f614
Jae hun Jin <ospf0306@gmail.com>
2025-04-01 19:58:20 +0900

    윤오꺼 가져옴

--- Assets/Scripts/UIScripts/InventoryUI.cs
+++ Assets/Scripts/UIScripts/InventoryUI.cs
@@ -11,45 +11,20 @@ public class InventoryUI : MonoBehaviour
     [SerializeField]
     private GameObject slotsParent;
     [SerializeField]
-    private TextMeshProUGUI timer;
+    private TextMeshProUGUI timer;      // 타이머
     [SerializeField]
-    private TextMeshProUGUI ammoText;
+    private TextMeshProUGUI ammoText;   // 탄창 수
 
     private Slot[] slots;
     private PlayerAnimator playerAnimator;
 
-    [Header("Current Weapon")]
-    [SerializeField]
-    private Image currentWeaponIcon;                // 현재 무기 아이콘
-    [SerializeField]
-    private TextMeshProUGUI currentWeaponName;      // 현재 무기 이름
-    [SerializeField]
-    private TextMeshProUGUI currentWeaponDetails;   // 현재 무기 상세정보
-    [SerializeField]
-    private TextMeshProUGUI currentWeaponStatus;    // 현재 무기 스탯
-    [SerializeField]
-    private GameObject panelDetail;                 // 무기 상세 정보 창
-
-    private UIWeapon currentWeapon;                 // 현재 무기
-
-    [Header("Holding Weapon List")]
-    [SerializeField]
-    private Image supportWeapon;        // 보조 무기
-    [SerializeField]
-    private Image mainWeapon_1;         // 메인 무기1
-    [SerializeField]
-    private Image mainWeapon_2;         // 메인 무기2
-    [SerializeField]
-    private Image gazet;                // 가젯
-
-    public Slot[] Slots => slots;
+    public Slot[] Slots => slots;       // 획득 아이템 리스트
 
     private void Awake()
     {
         slots = slotsParent.GetComponentsInChildren<Slot>();
         playerAnimator = FindObjectOfType<PlayerAnimator>();
         InventoryObject.SetActive(false);
-        panelDetail.SetActive(false);
     }
 
     private void Update()
@@ -66,7 +41,7 @@ public class InventoryUI : MonoBehaviour
             InventoryObject.SetActive(false);
         }
 
-        if(GameManager.instance.isGameStart)
+        if (GameManager.instance.isGameStart)
         {
             timer.text = $"{(int)GameManager.instance.Timer}";
             ammoText.text = $"{playerAnimator.Weapon.CurrentAmmo} / {playerAnimator.Weapon.MaxAmmo}";
@@ -96,40 +71,6 @@ public class InventoryUI : MonoBehaviour
             {
                 slots[i].AddItem(_item, _count);
                 return;
-            }
-        }
-    }
-
-    public void UpdateCurrentWeapon(UIWeapon current)
-    {
-        panelDetail.SetActive(true);
-        currentWeapon = current;
-
-        currentWeaponIcon.sprite = current.Icon;
-        currentWeaponName.text = current.Name;
-        currentWeaponDetails.text = current.Details;
-        currentWeaponStatus.text = current.WeaponStatus;
-    }
-
-    public void AddWeapon(UIWeapon current)
-    {
-        GameManager.instance.holdingWeaponPrefabs.Add(current.WeaponPrefab);
-        if (current.WeaponType == WeaponAttribute.Support)
-        {
-            supportWeapon.sprite = current.Icon;
-            //GameManager.instance.holdingWeaponPrefabs[0] = current.WeaponPrefab;
-        }
-        else if (current.WeaponType == WeaponAttribute.Main)
-        {
-            if (GameManager.instance.holdingWeaponPrefabs.Count < 2)
-            {
-                mainWeapon_1.sprite = current.Icon;
-                //GameManager.instance.holdingWeaponPrefabs[1] = current.WeaponPrefab;
-            }
-            else if (GameManager.instance.holdingWeaponPrefabs.Count < 3)
-            {
-                mainWeapon_2.sprite = current.Icon;
-                //GameManager.instance.holdingWeaponPrefabs[2] = current.WeaponPrefab;
             }
         }
     }
