71d2c76d4df64169654b64af038fa26f345ec2c1
Jae hun Jin <ospf0306@gmail.com>
2025-04-11 11:27:08 +0900

    수정 1차

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -8,46 +8,10 @@ public class Enemy : MonoBehaviour
 
     Rigidbody rigid;
     CapsuleCollider capsuleCollider;
-    Material mat;
-
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         capsuleCollider = GetComponent<CapsuleCollider>();
-        mat = GetComponent<MeshRenderer>().material;
     }
-
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.gameObject.tag == "CrowBar")
-        {
-            // 몬스터가 크로우바에 맞았을때 처리는 이곳에
-            // 예시
-            /*
-            Weapon weapon = collision.GetComponent<Weapon>();
-            curHealth -= weapon.damage;
-            */
-            curHealth -= 1;
-            StartCoroutine(OnDamage());
-        }
-    }
-
-    IEnumerator OnDamage()
-    {
-        mat.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-
-        if (curHealth > 0)
-        {
-            mat.color = Color.white;
-        }
-        else
-        {
-            mat.color = Color.gray;
-            gameObject.layer = 15;
-            Destroy(gameObject, 4);
-        }
-    }
-
 }
