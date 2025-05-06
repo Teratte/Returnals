6c6efdf7239b27038338d470b838c39a5a67a912
yuno0271 <yuno0271@naver.com>
2025-05-06 14:35:52 +0900

    스태미나 버그 수정 및 UI스프라이트 추가

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
         status.PlayerHP = Mathf.Clamp(status.PlayerHP,0,status.MaxHP);
 
         // 현재 스태미나가 남아있고, 이동 속도가 2보다 클 때(달릴 때)
-        if (status.MoveSpeed > 2.0f && !status.OnAdrenaline)
+        if (status.MoveSpeed > 2.0f && !status.OnAdrenaline && (Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f))
         {
             status.PlayerStamina -= Time.deltaTime * 10.0f;
         }
