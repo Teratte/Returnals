04a50b0f3c15c82b0ed389c77702b7a55c31f07e
Jae hun Jin <ospf0306@gmail.com>
2025-05-11 18:51:33 +0900

    폴더 관리 에셋 추가, 씬 수정, 몬스터 수정 (2)

--- Assets/Scripts/Enemys/EnemyData.cs
+++ Assets/Scripts/Enemys/EnemyData.cs
@@ -13,30 +13,29 @@ public class EnemyData : ScriptableObject
     public float moveSpeed = 3.5f; // 이동 속도
     public float rotationSpeed = 5f; // 회전 속도
     public float attackRange = 1f; // 공격 범위
-    public float attackDelay = 1.5f;  // 공격 간 딜레이
+    public float detectionRange = 10f; // 탐지 범위 (플레이어를 감지하는 범위, 정면 인식 각도와 함께 사용됨)
     public float defense = 1.5f; // 방어력
     public float monsterDamage = 40.0f; // 몬스터 공격력
-    public float postAttackDelay = 0.3f; // 공격 후 딜레이
+    public float attackDelay = 1.5f; // 공격 후 딜레이 (한번 공격 후 딜레이[경직]상태)
 
     [Header("공격 타입")]
     public EnemyAttackType enemyAttackType = EnemyAttackType.Melee;
 
-    /* 이 친구들은 FSM 소스코드로
-    private float curHealth;
-    private bool isPostDelay = false;
-    */
+    [Header("애니메이션")]
+    public AnimationClip idleAnimation; // 대기 애니메이션
+    public AnimationClip patrolAnimation;   // 순찰 애니메이션   
+    public AnimationClip chaseAnimation;    // 추적 애니메이션
+    public AnimationClip attackAnimation;   // 공격 애니메이션
+    public AnimationClip dieAnimation;  //  사망 애니메이션
 
-    [Header("Detection")][Tooltip("몬스터의 인식범위(시야각을 설정)")]
-    [SerializeField] public float detectionAngle = 90f; // 정면 인식 각도
+    [Header("몬스터 정면 시야 각도")][Tooltip("몬스터의 인식범위(시야각을 설정)")]
+    public float detectionAngle = 90f; // 정면 인식 각도
 
-    [Header("Weapon")][Tooltip("몬스터의 무기를 등록")] 
-    [SerializeField] public BoxCollider weaponCollider; // 몬스터의 무기 콜라이더
+    [Header("몬스터 무기")][Tooltip("몬스터의 무기를 등록")] 
+    public BoxCollider weaponCollider; // 몬스터의 무기 콜라이더
 
-    [Header("Item List")][Tooltip("몬스터 아이템 드랍")]
-    [SerializeField]
+    [Header("아이템 리스트")][Tooltip("몬스터 아이템 드랍")]
     public List<Item> itemList; // 드롭할 아이템 리스트
-    [SerializeField]
     public Item monsterIngredient; // 드롭할 몬스터 재료
-    [SerializeField]
     public float DropPercent;      // 드롭 확률
 }
--- Assets/Scripts/Enemys/EnemyFSM.cs
+++ Assets/Scripts/Enemys/EnemyFSM.cs
@@ -1,3 +1,4 @@
+using GLTF.Schema;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,12 +20,13 @@ public class EnemyFSM : MonoBehaviour, IDamageable
     private Rigidbody rigid;
     private CapsuleCollider capsuleCollider;
 
+    private string currentPlayingAnimation = string.Empty; // 현재 재생중인 애니메이션 이름
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void Awake()
     {
         curHealth = enemyData.maxHealth; // 최대 체력으로 초기화
+
         nav = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
@@ -37,15 +39,21 @@ public class EnemyFSM : MonoBehaviour, IDamageable
         nav.obstacleAvoidanceType = ObstacleAvoidanceType.HighQualityObstacleAvoidance; // 장애물 회피 타입 설정(고품질 회피 사용)
         nav.avoidancePriority = Random.Range(30, 70); // 장애물 회피 우선순위 설정(랜덤값으로 설정), 낮을 수록 우선순위가 높음, 여러 AI가 몰릴 때 충돌 방지
     }
+
     private void Start()
     {
         currentState = EnemyState.Idle; // 상태 초기화
+        
+        if (enemyData.weaponCollider != null) // 무기 콜라이더가 있다면 비활성화 (비벼져서 데미지 받는걸 방지)
+            enemyData.weaponCollider.enabled = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (currentState == EnemyState.None) return; // 상태 없으면 애니메이션 재생x, 자원 낭비 방지
 
+        StateUpdate(); // 상태 업데이트
     }
 
     private void FixedUpdate()
@@ -54,10 +62,31 @@ public class EnemyFSM : MonoBehaviour, IDamageable
 
     private void LateUpdate()
     {
-        
+        if(currentState == EnemyState.None) return; // 상태 없으면 애니메이션 재생x, 자원 낭비 방지
+        UpdateAnimation(); // 애니메이션 업데이트
     }
 
-
+    private void StateUpdate()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Patrol:
+                Patrol();
+                break;
+            case EnemyState.Chase:
+                Chase();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Die:
+                Die();
+                break;
+        }
+    }
 
     public void OnDamage(float damage)
     {
@@ -69,21 +98,45 @@ public class EnemyFSM : MonoBehaviour, IDamageable
 
         if (curHealth <= 0)
         {
-            Die();
-
+            currentState = EnemyState.Die; // 사망 상태로 전환
         }
     }
+
     void Idle()
     {
         // 대기 상태 처리
+        /*
+         [대기 방법]
+         대기 상태에서 2~3초 대기 후 현재 상태를 Patrol 상태로 전환 [else]
+         대기 중에 플레이어가 감지되면 현재상태를 Chase 상태로 전환
+         플레이어가 감지되는 조건 : 정면시야각도 및 탐지범위 내에서 플레이어가 감지되면 Chase 상태로 전환
+         만약 이때 공격 사거리 안에 들어와 있으면 Attack 상태로 전환
+         몬스터가 감지하는 범위 밖에서 플레이어가 먼저 공격하여 피격된 경우, 몬스터가 맞은 부위에 따라 그 방향으로 회전 및 이동
+         */
+
     }
     void Patrol()
     {
         // 순찰 상태 처리
+        /*
+         [순찰 방법]
+         임의 위치 순찰하는 방식
+         임의 위치로 2~3초 이동 후 Idle 상태로 전환
+         순찰 중 플레이어가 감지되면 현재 상태를 Chase 상태로 전환
+         플레이어가 감지되는 조건 : 정면시야각도 및 탐지범위 내에서 플레이어가 감지되면 Chase 상태로 전환
+         만약 이때 공격 사거리 안에 들어와 있으면 Attack 상태로 전환
+         몬스터가 감지하는 범위 밖에서 플레이어가 먼저 공격하여 피격된 경우, 몬스터가 맞은 부위에 따라 그 방향으로 회전 및 3초 이동
+         */
     }
     void Chase()
     {
         // 추격 상태 처리
+        /*
+         [추격 방법]
+         플레이어를 추격, 이때 이동 속도 증가(추격 속도)
+         만약 이때 공격 사거리 안에 들어와 있으면 Attack 상태로 전환
+         몬스터가 감지하는 범위(정면 시야 각 및 탐지 범위)를 플레이어가 벗어나면 Idle 상태로 전환
+         */
     }
     void Attack()
     {
@@ -101,12 +154,68 @@ public class EnemyFSM : MonoBehaviour, IDamageable
         }
     }
 
+    private void DropItem()
+    {
+        float percent = Random.Range(0, 100);
+        if (percent >= 100 - enemyData.DropPercent)
+        {
+            int itemIndex = Random.Range(0, enemyData.itemList.Count);
+            Instantiate(enemyData.itemList[itemIndex].itemPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    // 사망 처리
     void Die()
     {
-        // 사망 처리
+        gameObject.layer = 15;  // 죽은 오브젝트 레이어
+
+        nav.enabled = false;
+        capsuleCollider.enabled = false;
+        rigid.isKinematic = true;
+
+        StartCoroutine(DieRoutine());
     }
+    IEnumerator DieRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+        DropItem();
+    }
+
+    private void PlayAnimation(AnimationClip _clip, float _blendTime = 0.1f)
+    {
+        if (currentPlayingAnimation == _clip.name) return;  // 중복 재생 방지
+
+        anim.CrossFade(_clip.name, _blendTime);             // 애니메이션 재생
+        currentPlayingAnimation = _clip.name;               // 현재 재생중인 애니메이션 이름 저장
+    }
+
+    private void UpdateAnimation()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                PlayAnimation(enemyData.idleAnimation);   // Idle 애니메이션 재생
+                break;
+            case EnemyState.Patrol:
+                PlayAnimation(enemyData.patrolAnimation); // Patrol 애니메이션 재생
+                break;
+            case EnemyState.Chase:
+                PlayAnimation(enemyData.chaseAnimation);   // Chase 애니메이션 재생
+                break;
+            case EnemyState.Attack:
+                PlayAnimation(enemyData.attackAnimation); // Attack 애니메이션 재생
+                break;
+            case EnemyState.Die:
+                PlayAnimation(enemyData.dieAnimation);    // Die 애니메이션 재생
+                currentState = EnemyState.None;           // 사망 애니메이션 재생 후 State를 None으로
+                break;
+        }
+    }
+
     public enum EnemyState
     {
+        None = -1,
         Idle,
         Patrol,
         Chase,
