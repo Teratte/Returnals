b7bfb13c9cb16c268f09124bd0bb2a07d6948b5f
yuno0271 <yuno0271@naver.com>
2025-05-12 12:42:09 +0900

    오브젝트 표시

---
+++ Assets/Scripts/Handler/OnMousePointer.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnMousePointer : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer[] meshRenderers;
+    [SerializeField]
+    private TextMeshProUGUI interactionText;
+    [SerializeField]
+    private string objectInformation;
+    private Color originalColor;
+    private Color newColor = Color.red;
+
+    private void Awake()
+    {
+        originalColor = meshRenderers[0].material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        if(!GameManager.instance.isUIOn)
+        {
+            foreach (var renderer in meshRenderers)
+            {
+                renderer.material.color = newColor;
+            }
+
+            //Vector3 mousePos = Input.mousePosition;
+            interactionText.text = objectInformation;
+            //interactionText.transform.position = mousePos;
+            interactionText.enabled = true;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        interactionText.transform.position = mousePos;
+    }
+
+    private void OnMouseExit()
+    {
+        foreach (var renderer in meshRenderers)
+        {
+            renderer.material.color = originalColor;
+        }
+
+        interactionText.enabled = false;
+    }
+}
